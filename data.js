796936e2738907d4bed1a18f536aaaba1a4d77d3
Veer Pratap Singh <veerprataps.ranawat@gmail.com>
2025-04-11 16:01:56 +0530

    Update data.js

--- data.js
+++ data.js
@@ -1,136 +1,146 @@
 export const users = [
-    {
-      name: "Aanya Aroshree",
-      reg: "24BCA7571",
-      email: "aanya.24bca7571@vitapstudent.ac.in",
-      department: "EVENT MANAGEMENT",
-      groupLink: "https://chat.whatsapp.com/DDsTRQETa1x7y5c1AEcFcw"
-    },
-    {
-      name: "Bolisetty Lakshmi Harshitha",
-      reg: "24BCA8008",
-      email: "harshitha.24bca8008@vitapstident.ac.in",
-      department: "EVENT MANAGEMENT",
-      groupLink: "https://chat.whatsapp.com/DDsTRQETa1x7y5c1AEcFcw"
-    },
-    {
-      name: "Ch. Nikhitha Sri Devi",
-      reg: "24MIS7341",
-      email: "devi.24mis7341@vitapstudent.ac.in",
-      department: "EVENT MANAGEMENT",
-      groupLink: "https://chat.whatsapp.com/DDsTRQETa1x7y5c1AEcFcw"
-    },
-    {
-      name: "Keerthana",
-      reg: "24BEV7036",
-      email: "keerthana.24bev7036@vitapstudent.ac.in",
-      department: "EVENT MANAGEMENT",
-      groupLink: "https://chat.whatsapp.com/DDsTRQETa1x7y5c1AEcFcw"
-    },
-    {
-      name: "Mamidi Samitha",
-      reg: "24MIC7112",
-      email: "samitha.24mic7112@vitapstudent.ac.in",
-      department: "EVENT MANAGEMENT",
-      groupLink: "https://chat.whatsapp.com/DDsTRQETa1x7y5c1AEcFcw"
-    },
-    {
-      name: "Preetham",
-      reg: "24MIS7221",
-      email: "preetham.24mis7221@vitapstudent.ac.in",
-      department: "EVENT MANAGEMENT",
-      groupLink: "https://chat.whatsapp.com/DDsTRQETa1x7y5c1AEcFcw"
-    },
-    {
-      name: "Soumik Paul",
-      reg: "24BEC7010",
-      email: "soumik.24bec7010@vitapstudent.ac.in",
-      department: "EVENT MANAGEMENT",
-      groupLink: "https://chat.whatsapp.com/DDsTRQETa1x7y5c1AEcFcw"
-    },
-    {
-      name: "Aagman Sharma",
-      reg: "24BCB7031",
-      email: "aagman.24bcb7031@vitapstudent.ac.in",
-      department: "Marketing",
-      groupLink: "https://chat.whatsapp.com/CyMkB4KtGBxB8cNj87VLEz"
-    },
-    {
-      name: "Dewansh Wagh",
-      reg: "24BCE8079",
-      email: "dewansh.24bce8079@vitapstudent.ac.in",
-      department: "Marketing",
-      groupLink: "https://chat.whatsapp.com/CyMkB4KtGBxB8cNj87VLEz"
-    },
-    {
-      name: "Sucharita Sai Palli",
-      reg: "24BES7051",
-      email: "sucharita.24bes7051@vitapstudent.ac.in",
-      department: "Marketing",
-      groupLink: "https://chat.whatsapp.com/CyMkB4KtGBxB8cNj87VLEz"
-    },
-    {
-      name: "Ved Prakash Yadav",
-      reg: "24MIC7027",
-      email: "ved.24mic7027@vitapstudent.ac.in",
-      department: "Marketing",
-      groupLink: "https://chat.whatsapp.com/CyMkB4KtGBxB8cNj87VLEz"
-    },
-    {
-      name: "Aditya Sharma",
-      reg: "24BCE8057",
-      email: "aditya.24bce8057@vitapstudent.ac.in",
-      department: "Marketing",
-      groupLink: "https://chat.whatsapp.com/CyMkB4KtGBxB8cNj87VLEz"
-    },
-    {
-      name: "MOHAMMED ARSH KHAN",
-      reg: "24BCS7028",
-      email: "arsh.24bcs7028@vitapstudent.ac.in",
-      department: "Social Media",
-      groupLink: "https://chat.whatsapp.com/FoDs79jOfnt3QXOz7KCr8u"
-    },
-    {
-      name: "Aradhya Nautiyal",
-      reg: "23BCB7040",
-      email: "aradhya.23bcb7040@vitapstudent.ac.in",
-      department: "Technical",
-      groupLink: "https://chat.whatsapp.com/Cvd4JlRkymT4uLiF7MOgC0"
-    },
-    {
-      name: "Aswin",
-      reg: "23MIC7186",
-      email: "aswin.23mic7186@vitapstudent.ac.in",
-      department: "Technical",
-      groupLink: "https://chat.whatsapp.com/Cvd4JlRkymT4uLiF7MOgC0"
-    },
-    {
-      name: "MAHITHA VARANASI",
-      reg: "23BCE9914",
-      email: "mahitha.23bce9914@vitapstudent.ac.in",
-      department: "Technical",
-      groupLink: "https://chat.whatsapp.com/Cvd4JlRkymT4uLiF7MOgC0"
-    },
-    {
-      name: "Rahul T P",
-      reg: "24BCE7939",
-      email: "rahul.24bce7939@vitapstudent.ac.in",
-      department: "Technical",
-      groupLink: "https://chat.whatsapp.com/Cvd4JlRkymT4uLiF7MOgC0"
-    },
-    {
-      name: "Veer Pratap Singh",
-      reg: "23BCE7453",
-      email: "pratap.23bce7453@vitapstudent.ac.in",
-      department: "Technical",
-      groupLink: "https://chat.whatsapp.com/Cvd4JlRkymT4uLiF7MOgC0"
-    },
-     {
-      name: "Samradh Agarwal",
-      reg: "23BCE9250",
-      email: "samradh.23bce9250@vitapstudent.ac.in",
-      department: "Technical",
-      groupLink: "https://chat.whatsapp.com/Cvd4JlRkymT4uLiF7MOgC0"
-    }
-  ];
-  
+  {
+    name: "Aanya Aroshree",
+    reg: "24BCA7571",
+    email: "aanya.24bca7571@vitapstudent.ac.in",
+    department: "EVENT MANAGEMENT",
+    groupLink: "https://chat.whatsapp.com/DDsTRQETa1x7y5c1AEcFcw",
+    password: "Aro@471vB!x"
+  },
+  {
+    name: "Bolisetty Lakshmi Harshitha",
+    reg: "24BCA8008",
+    email: "harshitha.24bca8008@vitapstident.ac.in",
+    department: "EVENT MANAGEMENT",
+    groupLink: "https://chat.whatsapp.com/DDsTRQETa1x7y5c1AEcFcw",
+    password: "Har@008Lp$z"
+  },
+  {
+    name: "Ch. Nikhitha Sri Devi",
+    reg: "24MIS7341",
+    email: "devi.24mis7341@vitapstudent.ac.in",
+    department: "EVENT MANAGEMENT",
+    groupLink: "https://chat.whatsapp.com/DDsTRQETa1x7y5c1AEcFcw",
+    password: "Nik@341vC#d"
+  },
+  {
+    name: "Keerthana",
+    reg: "24BEV7036",
+    email: "keerthana.24bev7036@vitapstudent.ac.in",
+    department: "EVENT MANAGEMENT",
+    groupLink: "https://chat.whatsapp.com/DDsTRQETa1x7y5c1AEcFcw",
+    password: "Kee@036bX!q"
+  },
+  {
+    name: "Mamidi Samitha",
+    reg: "24MIC7112",
+    email: "samitha.24mic7112@vitapstudent.ac.in",
+    department: "EVENT MANAGEMENT",
+    groupLink: "https://chat.whatsapp.com/DDsTRQETa1x7y5c1AEcFcw",
+    password: "Sam@112bM%g"
+  },
+  {
+    name: "Preetham",
+    reg: "24MIS7221",
+    email: "preetham.24mis7221@vitapstudent.ac.in",
+    department: "EVENT MANAGEMENT",
+    groupLink: "https://chat.whatsapp.com/DDsTRQETa1x7y5c1AEcFcw",
+    password: "Pre@221cX&d"
+  },
+  {
+    name: "Soumik Paul",
+    reg: "24BEC7010",
+    email: "soumik.24bec7010@vitapstudent.ac.in",
+    department: "EVENT MANAGEMENT",
+    groupLink: "https://chat.whatsapp.com/DDsTRQETa1x7y5c1AEcFcw",
+    password: "Sou@010zP#k"
+  },
+  {
+    name: "Aagman Sharma",
+    reg: "24BCB7031",
+    email: "aagman.24bcb7031@vitapstudent.ac.in",
+    department: "Marketing",
+    groupLink: "https://chat.whatsapp.com/CyMkB4KtGBxB8cNj87VLEz",
+    password: "Aag@031vT$m"
+  },
+  {
+    name: "Dewansh Wagh",
+    reg: "24BCE8079",
+    email: "dewansh.24bce8079@vitapstudent.ac.in",
+    department: "Marketing",
+    groupLink: "https://chat.whatsapp.com/CyMkB4KtGBxB8cNj87VLEz",
+    password: "Dew@079yC!r"
+  },
+  {
+    name: "Sucharita Sai Palli",
+    reg: "24BES7051",
+    email: "sucharita.24bes7051@vitapstudent.ac.in",
+    department: "Marketing",
+    groupLink: "https://chat.whatsapp.com/CyMkB4KtGBxB8cNj87VLEz",
+    password: "Suc@051rQ@e"
+  },
+  {
+    name: "Ved Prakash Yadav",
+    reg: "24MIC7027",
+    email: "ved.24mic7027@vitapstudent.ac.in",
+    department: "Marketing",
+    groupLink: "https://chat.whatsapp.com/CyMkB4KtGBxB8cNj87VLEz",
+    password: "Ved@027mX#f"
+  },
+  {
+    name: "Aditya Sharma",
+    reg: "24BCE8057",
+    email: "aditya.24bce8057@vitapstudent.ac.in",
+    department: "Marketing",
+    groupLink: "https://chat.whatsapp.com/CyMkB4KtGBxB8cNj87VLEz",
+    password: "Adi@057bK%z"
+  },
+  {
+    name: "MOHAMMED ARSH KHAN",
+    reg: "24BCS7028",
+    email: "arsh.24bcs7028@vitapstudent.ac.in",
+    department: "Social Media",
+    groupLink: "https://chat.whatsapp.com/FoDs79jOfnt3QXOz7KCr8u",
+    password: "Ars@028pW&m"
+  },
+  {
+    name: "Aradhya Nautiyal",
+    reg: "23BCB7040",
+    email: "aradhya.23bcb7040@vitapstudent.ac.in",
+    department: "Technical",
+    groupLink: "https://chat.whatsapp.com/Cvd4JlRkymT4uLiF7MOgC0",
+    password: "Ara@040qZ#x"
+  },
+  {
+    name: "Aswin",
+    reg: "23MIC7186",
+    email: "aswin.23mic7186@vitapstudent.ac.in",
+    department: "Technical",
+    groupLink: "https://chat.whatsapp.com/Cvd4JlRkymT4uLiF7MOgC0",
+    password: "Asw@186yX$d"
+  },
+  {
+    name: "MAHITHA VARANASI",
+    reg: "23BCE9914",
+    email: "mahitha.23bce9914@vitapstudent.ac.in",
+    department: "Technical",
+    groupLink: "https://chat.whatsapp.com/Cvd4JlRkymT4uLiF7MOgC0",
+    password: "Mah@914zV*w"
+  },
+  {
+    name: "Rahul T P",
+    reg: "24BCE7939",
+    email: "rahul.24bce7939@vitapstudent.ac.in",
+    department: "Technical",
+    groupLink: "https://chat.whatsapp.com/Cvd4JlRkymT4uLiF7MOgC0",
+    password: "Rah@939bW!p"
+  },
+  {
+    name: "Veer Pratap Singh",
+    reg: "23BCE7453",
+    email: "pratap.23bce7453@vitapstudent.ac.in",
+    department: "Technical",
+    groupLink: "https://chat.whatsapp.com/Cvd4JlRkymT4uLiF7MOgC0",
+    password: "Vee@453zT#s"
+  }
+];
